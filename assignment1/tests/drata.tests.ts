0542c4985b94daa7b78281d7a1247706293f0764
jbeck2099 <109643107+jbeck2099@users.noreply.github.com>
2024-01-15 15:44:54 -0700

    Update drata.tests.ts

--- assignment1/tests/drata.tests.ts
+++ assignment1/tests/drata.tests.ts
@@ -3,9 +3,9 @@ import DrataPage from '../pages/drataPage';
 let settings = require('../data/settings.json');
 let links: string[] = [];
 let visitedLinks: string[] = [];
-let linkNumber: number = 0;
+// let linkNumber: number = 0;
 
-test('Test Home Page & Get All Links', async({page}) => {
+test('Test home page & get all links', async({page}) => {
     let drataPage = new DrataPage(page);
     await drataPage.navigateHome();
     await drataPage.validateHomePageLinkExists();
@@ -13,10 +13,50 @@ test('Test Home Page & Get All Links', async({page}) => {
 
     links = await page.evaluate(() => {
         return Array.from(document.links).map(item => item.href);
-    });
+    });    
 });
 
-// test.describe(``, () => {
+test.describe('Test all page links', () => {    
+    for (const link of links){
+        if (!link.includes(settings.homePageUrl)){ //skip links to external sites
+            continue;
+        }
+
+        if (visitedLinks.includes(link)){ //skip repeated links
+            continue;
+        }
+        else{
+            visitedLinks.push(link);
+        }
+
+        test(`Testing page @ '${link}'`, async({page}) => {
+            let drataPage = new DrataPage(page);
+            await drataPage.navigateToPage(link);
+            await drataPage.validateHomePageLinkExists();
+            await drataPage.validateNoConsoleErrors();
+        });
+    }
+
+    // for (let i = 0; i < links.length; i++){
+    //     if (!links[i].includes(settings.homePageUrl)){ //skip links to external sites
+    //         continue;
+    //     }
+
+    //     if (visitedLinks.includes(links[i])){ //skip repeated links
+    //         continue;
+    //     }
+    //     else{
+    //         visitedLinks.push(links[i]);
+    //     }
+
+    //     test(`Testing link ${i} (${links[i]})`, async({page}) => {
+    //         let drataPage = new DrataPage(page);
+    //         await drataPage.navigateToPage(links[i]);
+    //         await drataPage.validateHomePageLinkExists();
+    //         await drataPage.validateNoConsoleErrors();
+    //     });
+    // }
+
     // links.forEach((link) => {
     //     linkNumber++;
         
@@ -38,24 +78,4 @@ test('Test Home Page & Get All Links', async({page}) => {
     //         await drataPage.validateNoConsoleErrors();
     //     });
     // });
-    
-    for (let i = 0; i < links.length; i++){
-        if (!links[i].includes(settings.homePageUrl)){ //skip links to external sites
-            continue;
-        }
-
-        if (visitedLinks.includes(links[i])){ //skip repeated links
-            continue;
-        }
-        else{
-            visitedLinks.push(links[i]);
-        }
-
-        test(`Testing link ${i} (${links[i]})`, async({page}) => {
-            let drataPage = new DrataPage(page);
-            await drataPage.navigateToPage(links[i]);
-            await drataPage.validateHomePageLinkExists();
-            await drataPage.validateNoConsoleErrors();
-        });
-    }
-// });
+});
