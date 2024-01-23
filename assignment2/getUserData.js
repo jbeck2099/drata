cbcadaad90b426f1e21ccf4cf506293d7f0af84d
jbeck2099 <109643107+jbeck2099@users.noreply.github.com>
2024-01-23 14:28:05 -0700

    updates to assignment 2

--- assignment2/getUserData.js
+++ assignment2/getUserData.js
@@ -1,7 +1,22 @@
-import userData from './data/userData.json'
+const axios = require('axios')
+const userData = require('./data/userData.json')
+const responseData = require('./data/responseData.json')
+const settings = require('./data/settings.json')
 
-export function getUserData(username){
-    return userData.filter(user => {
-        user.username === username
-    })
+let getUserResponseData = (authToken) => {    
+    for (let i = 0; i < userData.length; i++){
+        if (userData[i]["authToken"] == authToken){
+            for (let j = 0; j < responseData.length; j++){
+                if (responseData[j]["login"] == userData[i]["username"]){
+                    return responseData[j];
+                }
+            }
+        }
+    }
 }
+
+let getAuthenticatedUser = async (authToken) => {
+    return await axios.get(settings.getAuthenticatedUserUrl, {'headers': {'Authorization': `Bearer ${authToken}`}});
+}
+
+module.exports = { getAuthenticatedUser, getUserResponseData };
--- assignment2/getUserData.test.js
+++ assignment2/getUserData.test.js
@@ -1,21 +1,69 @@
-import getUserData from './getUserData'
-import userData from './data/userData.json'
+const axios = require('axios')
+const settings = require('./data/settings.json')
+const { getUserResponseData, getAuthenticatedUser } = require('./getUserData.js')
+const userData = require('./data/userData.json')
+const plans = require('./data/plans.json')
 
-describe('Test info for list of accounts', () => {
-    for (const user of userData){
-        const response = getUserData(user.username);
-        
-        test(`Test userid for user '${user.username}'`, () => {
+jest.mock('axios')
+
+for (const user of userData){
+    describe(`Testing response info for user '${user.username}'`, () => {
+        test('Testing userid (number)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.id).toBeGreaterThanOrEqual(settings.planMinValue);
+            expect(response.id).toBeLessThanOrEqual(settings.planMaxValue);
             expect(response.id).toBe(user.userId);
         })
-
-        test(`Test plan for user '${user.username}'`, () => {
+        
+        test('Testing plan (string, array)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(plans).toContain(response.plan.name);
             expect(response.plan.name).toBe(user.planName);
         })
 
-        test(`Test url for user '${user.username}'`, () => {
-            expect(response.url).toBe(user.url);
+        test('Testing url (string)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.url).toBe(`${settings.userPersonalUrlPrefix}${user.username}`);
+            expect(response.url).toHaveLength(settings.userPersonalUrlPrefix.length + user.username.length);
         })
-    }
-})
 
+        test('Testing site admin status (bool)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.site_admin).toBeFalsy();
+        })
+
+        test('Testing hirable status (bool)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.hireable).toBeTruthy();
+        })
+
+        test('Testing gravatar id value (null)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.gravatar_id).toBeNull();
+        })
+
+        test('Testing disk usage (infinity)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.disk_usage).toBe(Infinity);
+        })
+
+        test('Testing following (NaN)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.following / 0).toBeNaN();
+        })
+
+        test('Testing non-existent element (undefined)', async () => {
+            axios.get.mockResolvedValue(getUserResponseData(user.authToken));
+            let response = await getAuthenticatedUser(user.authToken);
+            expect(response.fakeElement).toBeUndefined;
+        })
+    })
+}
